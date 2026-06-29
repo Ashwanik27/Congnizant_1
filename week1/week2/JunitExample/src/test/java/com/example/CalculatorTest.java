de4e6fadf58d473b7d2fa452ea2d0568a37a9834
Ashwani Kumar Verma <ashwani@example.com>
2026-06-29 22:33:46 +0530

    Complete JUnit Exercise 1

--- week1/week2/JunitExample/src/test/java/com/example/CalculatorTest.java
+++ week1/week2/JunitExample/src/test/java/com/example/CalculatorTest.java
@@ -1,5 +1,13 @@
 package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
-    
+
+    @Test
+    public void testAdd() {
+        Calculator c = new Calculator();
+        assertEquals(10, c.add(4, 6));
+    }
 }
